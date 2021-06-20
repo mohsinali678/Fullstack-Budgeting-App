2f1b1164b15a2ce25861eff6b5e7936408988d38
Hafiz Ali <mohsantahir@yahoo.com>
2021-06-20 13:37:11 -0400

    Created Controller and added Routes in app.js

--- budgeting-app-backend/app.js
+++ budgeting-app-backend/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 //CONFIGURATION
 const app = express();
-
+const transactionControllers = require("./controllers/transactionController.js");
 
 //MIDDLEWARE
 app.use(cors());
@@ -19,6 +19,14 @@ app.get("/", (req, res) => {
     res.send("Welcome to Budgeting App!");
 })
 
+//Route to Controller
+app.use("/transactions", transactionControllers);
+
+
+//Page not Found Route
+app.get("*", (req, res) => {
+    res.status(400).send("Page Not Found!");
+});
 
 //EXPORT
 module.exports = app;
