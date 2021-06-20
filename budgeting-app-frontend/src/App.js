f50a084f1133093bbacd25c549e779d5772973d3
Hafiz Ali <mohsantahir@yahoo.com>
2021-06-20 14:22:14 -0400

    Worked on Styling table

--- budgeting-app-frontend/src/App.js
+++ budgeting-app-frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { apiURL } from "./util/apiURL";
 import axios from "axios";
+import './App.css';
 
 //PAGES
 import Home from "./Pages/Home";
