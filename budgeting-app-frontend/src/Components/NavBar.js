c5ba3816bc1fa9f37859a0c5b88c810006762d06
Hafiz Ali <mohsantahir@yahoo.com>
2021-06-22 11:40:26 -0400

    Made minor changes in Navbar.js, apiURL.js and added _redirects

--- budgeting-app-frontend/src/Components/NavBar.js
+++ budgeting-app-frontend/src/Components/NavBar.js
@@ -4,12 +4,13 @@ function NavBar() {
     return (
         <nav>
             <h1>
-                <Link to="/transactions">BUDGET APPLICATION</Link>
+                <Link to="/">HOME</Link>
             </h1>
 
             <h1>
-                <Link to="/">HOME</Link>
+                <Link to="/transactions">BUDGET</Link>
             </h1>
+
 
             <button>
                 <Link style={{color:"#02007C", fontSize:"15px", fontWeight:"bolder"}} to="/transactions/new">New Transaction</Link>
